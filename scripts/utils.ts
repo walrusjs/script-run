2234caf112bcdb41fcce9e0eb39042d483c7d2e6
wangxingkang <156148958@qq.com>
2021-12-30 12:29:04 +0800

    release: independent

---
+++ scripts/utils.ts
@@ -0,0 +1,55 @@
+import { logger } from '@walrus/cli-utils';
+import { existsSync, readdirSync, readFileSync, writeFileSync } from 'fs';
+import { join } from 'path';
+
+export function getPkgs(opts?: { base?: string }): string[] {
+  const base = opts?.base || join(__dirname, '../packages');
+  return readdirSync(base).filter((dir) => {
+    return !dir.startsWith('.') && existsSync(join(base, dir, 'package.json'));
+  });
+}
+
+export function eachPkg(
+  pkgs: string[],
+  fn: (opts: { pkg: string; pkgPath: string }) => void,
+  opts?: { base?: string },
+) {
+  const base = opts?.base || join(__dirname, '../packages');
+  pkgs.forEach((pkg) => {
+    fn({
+      pkg,
+      pkgPath: join(base, pkg),
+    });
+  });
+}
+
+export function assert(v: unknown, message: string) {
+  if (!v) {
+    logger.error(message);
+    process.exit(1);
+  }
+}
+
+export function setExcludeFolder(opts: {
+  cwd: string;
+  pkg: string;
+  dirName?: string;
+  folders?: string[];
+}) {
+  const dirName = opts.dirName || 'packages';
+  const folders = opts.folders || ['dist', 'compiled'];
+  if (!existsSync(join(opts.cwd, '.idea'))) return;
+  const configPath = join(opts.cwd, '.idea', 'umi-next.iml');
+  let content = readFileSync(configPath, 'utf-8');
+  for (const folder of folders) {
+    const excludeContent = `<excludeFolder url='file://$MODULE_DIR$/${dirName}/${opts.pkg}/${folder}' />`;
+    const replaceMatcher = `<content url="file://$MODULE_DIR$">`;
+    if (!content.includes(excludeContent)) {
+      content = content.replace(
+        replaceMatcher,
+        `${replaceMatcher}\n      ${excludeContent}`,
+      );
+    }
+  }
+  writeFileSync(configPath, content, 'utf-8');
+}
